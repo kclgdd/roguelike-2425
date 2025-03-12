7af7c8f9a42cc9be0908422b24c60e6bebee0ad4
Conoisseur <97054370+Conoisseur@users.noreply.github.com>
2025-03-12 19:53:40 +0000

    feat: add timer

--- scenes/HUD.gd
+++ scenes/HUD.gd
@@ -3,6 +3,10 @@ extends Control
 @onready var canvas_layer = $Canvas
 @export var hearts : Array[Node]
 @export var collectable_label: Label
+@onready var timer_label: Label = $Canvas/TimerLabel
+@onready var timer: Timer = $Timer
+
+var elapsed_time: float = 0.0
 
 func _ready():
 	update_hearts_display()
@@ -10,11 +14,18 @@ func _ready():
 	
 	collectable_label.text = "Apples: 0"
 	GameManager.connect("collectible_changed", update_collectible_display)
-	
+
+	# Set up the timer
+	timer.wait_time = 1.0 
+	timer.one_shot = false
+	timer.start()
+	timer.timeout.connect(_on_timer_timeout)
+
+	timer_label.text = "00:00"
+
 func hide_hud():
 	canvas_layer.visible = false
 
-# Method to show the HUD
 func show_hud():
 	canvas_layer.visible = true
 			
@@ -27,4 +38,12 @@ func update_hearts_display():
 	
 func update_collectible_display():
 	collectable_label.text = "Apples: " + str(GameManager.collectable)
-	
+
+func _on_timer_timeout():
+	elapsed_time += 1.0  
+	timer_label.text = format_time(elapsed_time)
+
+func format_time(seconds: float) -> String:
+	var minutes = int(seconds) / 60
+	var secs = int(seconds) % 60
+	return "%02d:%02d" % [minutes, secs]
